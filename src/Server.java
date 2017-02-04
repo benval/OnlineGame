48476a7accc5726a3f1d75e2d689ccb96b2aa568
Marcus <Marcus@Ramberg>
2017-02-04 16:45:34 +0100

    exc

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,18 @@
+import java.net.ServerSocket;
 
 public class Server {
 
+	public static void main(String[] args) throws Exception{
+		ServerSocket listener = new ServerSocket(8902);
+		System.out.println("Server is running.");
+		try{
+			while(true) {
+				System.out.println("Hey");
+			}
+		} finally{
+			listener.close();
+		}
+		
+	}
+	
 }
