1c1a003171f2815cb079f653aa5b5a4d80302ac4
benval <Benjamin@10.22.112.163>
2017-02-04 17:01:54 +0100

    op

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,18 @@
+import java.net.Socket;
 
 public class Client {
+private Socket socket;
+private static int PORT = 8902;
 
-	public static void main(String[] args) {
+
+	public Client (String serverAddress) throws Exception {
+		socket = new Socket(serverAddress, PORT);
+	}
+	
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		String serverAddress = (args.length == 0) ? "localhost" : args[1];
+		Client gameClient = new Client(serverAddress);
 	}
 
 }
